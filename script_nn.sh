116b50cd3a174d7f09f0f6ffaf3d837b1ef856f0
CheshireCat12 <anthony.gillioz@outlook.com>
2023-01-26 13:19:16 +0100

    merge

--- script_nn.sh
+++ script_nn.sh
@@ -9,9 +9,9 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --time=4-00:00:00
 #SBATCH --output=/storage/homefs/ag21k209/neo_slurms/clf_nn_%A_%a.out
-#SBATCH --array=1-60
+#SBATCH --array=1-74
 
-param_store=./arguments_gk_spectral_big.txt
+param_store=./arguments_clf_nn.txt
 
 # Get first argument
 dataset_name=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
@@ -20,7 +20,7 @@ degree=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $2}'
 # Put your code below this line
 module load Workspace_Home
 module load Python/3.9.5-GCCcore-10.3.0.lua
-cd $HOME/graph_library/../
+cd $HOME/graph_library/graph_reduction/filter_reduction
 source venv/bin/activate
 
-srun python main_nn.py --root_dataset $SCRATCH/data/ --dataset $dataset_name $degree
+srun python main_nn.py --root-dataset $SCRATCH/tmp/ --dataset $dataset_name $degree
